0dbf5215161c1280df8203a77b2b866d182cc79b
Assen Kolov <assen.kolov@gmail.com>
2012-01-31 23:15:57 +0100

    Tests

--- src/x2j/core.clj
+++ src/x2j/core.clj
@@ -11,22 +11,24 @@
 
 (defn decorate-kwd "attach @ at the beginning of a keyword"
   [kw] (keyword (str "@" (subs (str kw) 1))))
-(defn decorate-atts " prepends @ to keys in a map("
+(defn decorate-attrs " prepends @ to keys in a map("
   [m] (zipmap (map decorate-kwd (keys m)) (vals m)))
 
 (defn build-content-seq   [attrs content]   
-  (merge  (if attrs (decorate-atts attrs) {})
+  (merge  (if attrs (decorate-attrs attrs) {})
           (cond  (map? (first content))  (reduce merge (map build-node content))
                  (nil? content) {}
                  :else (hash-map  "#text" (first content))
                  )))
   
 (defn build-content [{ attrs :attrs content :content}]                           
-                                  (if (and (nil? attrs) (nil? content)) nil             
-                                        (build-content-seq  attrs content)))
+ (if (and (nil? attrs) (nil? content)) nil             
+  (build-content-seq  attrs content)))
 
 (defn build-node [node] (hash-map (:tag node) (build-content node))) 
 
-(defn x2j [x] (j/generate-string (bild-node (xml-parse-string x))))
+(defn x2j [x] (j/generate-string (build-node (xml-parse-string x))))
+
+(defn see [x] (println (x2j x)))
 
 
--- test/x2j/test/core.clj
+++ test/x2j/test/core.clj
@@ -1,6 +1,30 @@
-(ns jx.test.core
-  (:use [jx.core])
-  (:use [clojure.test]))
+(ns x2j.test.core
+  (:use [x2j.core])
+  (:use [clojure.test])
+  (:use [clojure.string]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(defn dehyd [^String s] (replace (replace s " " "") "\"" "'"))
+
+(defn === [a b] "Compare ignoring whitespace. \" is same as '"
+  (= (dehyd a) (dehyd b)))
+
+(deftest eq1
+  (is (=== "a" "a "))
+  (is (=== "a" " a"))
+  (is (=== "a'b" "a\"b"))
+  (is (=== " a \" b  " "a     ' b"))
+  )
+
+
+
+(defmacro test-j-x
+  "Test that XML produces the expected JSON"
+  [name [j x]]
+  ( list `deftest name `(is (=== ~j (x2j ~x)))))
+
+         
+(deftest a  
+  (is (=== "{'a':null}" (x2j "<a/>"))))
+
+(test-j-x a1 [ "{ 'a':null}", "<a/>"])
+(test-j-x a2 [ "{ 'a':'x'}", "<a>x</a>"])
